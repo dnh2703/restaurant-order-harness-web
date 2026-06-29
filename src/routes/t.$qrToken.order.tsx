aaf0e71e971b50f61833ff7faaf589c24bb4bd2d
Johnny Dang <dnh2703@gmail.com>
2026-06-29 21:30:02 +0700

    feat(web): /t/:token/order route

---
+++ src/routes/t.$qrToken.order.tsx
@@ -0,0 +1,29 @@
+// src/routes/t.$qrToken.order.tsx
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { CustomerOrderPage } from '@/pages/customer-order/CustomerOrderPage'
+import { Button } from '@/shared/ui'
+
+export const Route = createFileRoute('/t/$qrToken/order')({
+  component: OrderRoute,
+})
+
+function OrderRoute() {
+  const { qrToken } = Route.useParams()
+  return (
+    <CustomerOrderPage
+      qrToken={qrToken}
+      backLink={
+        <Link
+          to="/t/$qrToken"
+          params={{ qrToken }}
+          search={{ q: '', cat: 'all' }}
+          className="block"
+        >
+          <Button variant="secondary" fullWidth>
+            Gọi thêm món
+          </Button>
+        </Link>
+      }
+    />
+  )
+}
